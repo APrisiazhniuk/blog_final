a4ff62b02efd7bfeb56275b3d32010a1c1b2cfb9
APrisiazhniuk <olexytank@gmail.com>
2021-09-02 11:28:14 +0300

    add send mail

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
+use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
+//        $password = Str::random(10);
+        Mail::to($data['email'])->send(new PasswordMail($data['password']));
         $data['password'] = Hash::make($data['password']);
-        User::firstOrCreate(['email' => $data['email']], $data);
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
+        event(new Registered($user));
         return redirect()->route('admin.user.index');
     }
 }
--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -51,7 +51,7 @@
                                         <td>{{$user->id}}</td>
                                         <td>{{$user->name}}</td>
                                         <td>{{$user->email}}</td>
-                                        <td>{{$user->role}}</td>
+                                        <td>{{$role}}</td>
                                     </tr>
                                     </tbody>
                                 </table>
